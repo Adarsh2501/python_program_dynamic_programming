64f41c5c0fe24b1903f86cda067dcab3c5b50510
Adarsh2501 <43618391+Adarsh2501@users.noreply.github.com>
2018-09-26 23:49:38 +0530

    update code.py
    appending a number directly to list is easier because when we execute on 32 bit systems, complex ways may generate errors. Taking input in desired format is much better than converting the input later, especially like integer.

--- code.py
+++ code.py
@@ -1,14 +1,12 @@
-n=input()
+n=int(input())
 list=[]
-n1=int(n)
-for i in range(n1):
-    k=input()
-    list=list+[int(k)]
+for i in range(n):
+    k=int(input())
+    list.append(k)
 ans=[]
 for k in range(n1):
-    ans=ans+[1]
+    ans.append(1)
 for m in range(n1-2,-1,-1):
-    
     for p in range(m+1,n1,+1):
         if(list[p]%list[m]==0):
             if 1+ans[p]>ans[m]:
